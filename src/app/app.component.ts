ff7a7ad78d532582287e8059bd98cb5a40b4082f
jtquangduy <justinquangduy@gmail.com>
2024-12-17 16:02:57 +0700

    Building a Custom Structural Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 
 import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
+import { AuthDirective } from './auth/auth.directive';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent],
+  imports: [AuthComponent, LearningResourcesComponent, AuthDirective],
 })
-export class AppComponent {}
+export class AppComponent {
+  private authService = inject(AuthService);
+
+  isAdmin = computed(() => this.authService.activePermission() === 'admin');
+}
